168ba750b14618cb626d1f2ce23592e7c7d4fdb1
Victor-994 <victoradejumo2@gmail.com>
2022-06-21 15:22:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+// take the operand input
+const numb1 = parseFloat(prompt('Enter first number: '));
+const op = prompt('Enter operator ( either +, -, * or / ): ');
+const numb2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (op == '+') {
+    result = numb1 + numb2;
+}
+else if (op == '-') {
+    result = numb1 - numb2;
+}
+else if (op == '*') {
+    result = numb1 * numb2;
+}
+else {
+    result = numb1 / numb2;
+}
+
+// display the result
+console.log(`${numb1} ${op} ${numb2} = ${result}`);
